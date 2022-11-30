ba9746daf72e8fdba576886337b516605f2f7993
hali2101 <hali2101@student.miun.se>
2022-11-30 10:40:05 +0100

    lagt till för att lägg till, och läsa enskild port

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,53 +2,107 @@
 
 let baseURL = "http://localhost:3000/courses/";
 
+//getting dom-elements for manipulation
+let msg = document.getElementById("msg");
+
 // Wait for DOM to load
-document.addEventListener("DOMContentLoaded", function(){ 
-//
-// Read and publish all courses 
-    let url = baseURL;
-    
-    //get the data from db
-    fetch(url, {method: 'GET'})
-        .then(response => response.text())
-        .then(data => {
-                
-                let jsonData = JSON.parse( data );
+document.addEventListener("DOMContentLoaded", function () {
+  //
+  // Read and publish all courses
+  let url = baseURL;
 
-                let s = "<div id=resptable><table><th>ID</th><th>Kurskod</th><th>Kursnamn</th><th>Kursperiod</th><th>Ta bort kurs</th>";
-                for(let i=0; i < jsonData.length; i++){
-                
-                //save data in different variables for cleaner output
-                let id = jsonData[i]._id;
-                let courseid = jsonData[i].courseId;
-                let coursename = jsonData[i].courseName;
-                let courseperiod = jsonData[i].coursePeriod;
+  //get the data from db
+  fetch(url, { method: "GET" })
+    .then((response) => response.text())
+    .then((data) => {
+      let jsonData = JSON.parse(data);
 
-                    s += `<tr><td>${id}</td><td>${courseid}</td><td>${coursename}</td><td>${courseperiod}</td><td><button class=erase id=${id}>Radera</button></td></tr>`;
-                    
-                }
-                s += "</table></div>";
+      let s =
+        "<div id=resptable><table><th>Kurskod</th><th>Kursnamn</th><th>Progression</th><th>Kursperiod</th><th>Kurslänk</th><th>Ta bort kurs</th>";
+      for (let i = 0; i < jsonData.length; i++) {
+        //save data in different variables for cleaner output
+        let id = jsonData[i]._id;
+        let courseid = jsonData[i].courseid;
+        let coursename = jsonData[i].coursename;
+        let progression = jsonData[i].progression;
+        let term = jsonData[i].term;
+        let syllabus = jsonData[i].syllabus;
 
-                document.getElementById("courselist").innerHTML = s;
-             })
-        .catch(error => {
-            alert('There was an error '+error);
+        s += `<tr><td>${courseid}</td><td>${coursename}</td><td>${progression}</td><td>${term}</td><td>${syllabus}</td><td><button class=erase id=${id}>Radera</button></td></tr>`;
+      }
+      s += "</table></div>";
+
+      document.getElementById("courselist").innerHTML = s;
+    })
+    .catch((error) => {
+      alert("There was an error " + error);
+    });
+
+  //Post new course to database
+  document
+    .getElementById("newcourse")
+    .addEventListener("submit", async function (e) {
+      e.preventDefault(e);
+
+      //hämta in från inputfälten
+      let courseid = document.getElementById("courseid").value;
+      let coursename = document.getElementById("coursename").value;
+      let progression = document.getElementById("progression").value;
+      let term = document.getElementById("term").value;
+      let syllabus = document.getElementById("syllabus").value;
+
+      //check if input is empty
+      if (
+        courseid != "" &&
+        coursename != "" &&
+        progression != "" &&
+        term != "" &&
+        syllabus != ""
+      ) {
+        //save as object
+        let coursebody = {
+          courseid: courseid,
+          coursename: coursename,
+          progression: progression,
+          term: term,
+          syllabus: syllabus,
+        };
+        //make fetch to post to database
+        fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          //stringify body to database
+          body: JSON.stringify(coursebody),
         });
+        //empty inputfields
+        courseid = "";
+        coursename = "";
+        progression = "";
+        term = "";
+        syllabus = "";
 
-//
-// Create event handler for delete course
-document.getElementById("courselist").addEventListener("click", function(e){
+        //page reload with new content
+        location.reload();
+      } else {
+        msg.innerHTML =
+          "Något gick fel, var god fyll i alla fält för att lägga till kurs.";
+      }
+    });
+
+  // Create event handler for delete course
+  document.getElementById("courselist").addEventListener("click", function (e) {
     let url = baseURL + e.target.id;
     //Delete a course depending on ID
-    fetch(url, {method: 'DELETE'})
-        .then(response => response.text())
-        .then(data => {
-                //reload the page with new content
-                location.reload();
-             })
-        .catch(error => {
-            alert('There was an error '+error);
-        });
-});
-
+    fetch(url, { method: "DELETE" })
+      .then((response) => response.text())
+      .then((data) => {
+        //reload the page with new content
+        location.reload();
+      })
+      .catch((error) => {
+        alert("There was an error " + error);
+      });
+  });
 });
--- routes/courses.js
+++ routes/courses.js
@@ -1,100 +1,106 @@
-var express = require('express');
-//creating an instance of the app
+var express = require("express");
+//creating an instance of the router
 var router = express.Router();
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 router.use(
-    bodyParser.urlencoded({
-      extended: true
-    })
-)
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 router.use(bodyParser.json());
 
 /******* Connect to database with mongoose ********/
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/myCV', { useNewUrlParser: true, useUnifiedTopology: true });
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/myCV", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 mongoose.Promise = global.Promise; // Global use of mongoose
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on("error", console.error.bind(console, "connection error:"));
 
-db.once('open', function (callback) { // Add the listener for db events 
-	console.log("Connected to db");
+db.once("open", function (callback) {
+  // Add the listener for db events
+  console.log("Connected to db");
 
-	// Create DB scheme 
-	var courseSchema = mongoose.Schema({
-		courseid: String,
-		coursename: String,
-		progression: String,
-		term: String,
-		syllabus: String
-	});
+  // Create DB scheme
+  var courseSchema = mongoose.Schema({
+    courseid: String,
+    coursename: String,
+    progression: String,
+    term: String,
+    syllabus: String,
+  });
 
-	// Create scheme model
-	var Course = mongoose.model('Course', courseSchema)
+  // Create scheme model
+  var Course = mongoose.model("Course", courseSchema);
 
-	/******* Get all courses ********/
-	router.get('/', function (req, res, next) {
+  /******* Get all courses ********/
+  router.get("/", function (req, res, next) {
+    // Läs ut från databasen
+    Course.find(function (err, courses) {
+      if (err) return console.error(err);
+      var jsonObj = JSON.stringify(courses);
+      res.contentType("application/json");
+      res.send(jsonObj);
+    });
+  });
 
-		// Läs ut från databasen
-		Course.find(function (err, courses) {
-			if (err) return console.error(err);
-			var jsonObj = JSON.stringify(courses);
-			res.contentType('application/json');
-			res.send(jsonObj);
-		});
-	});
-	
+  /******* GET single course with ID ********/
+  router.get("/:id", function (req, res, next) {
+    var id = req.params.id;
 
-	/******* GET single course with ID ********/
-	router.get('/:id', function (req, res, next) {
+    Course.findById(id, function (err, courses) {
+      if (err) return console.error(err);
+      var jsonObj = JSON.stringify(courses);
+      res.contentType("application/json");
+      res.send(jsonObj);
+    });
+  });
 
+  /******* ADD new course to database-list ********/
+  router.post("/", function (req, res, next) {
+    // Create new course
+    var coursebody = new Course({
+      courseid: req.body.courseid,
+      coursename: req.body.coursename,
+      progression: req.body.progression,
+      term: req.body.term,
+      syllabus: req.body.syllabus,
+    });
 
-	});
+    console.log("test" + req.body.coursename);
 
-	/******* ADD new course to database-list ********/
-	router.post('/', function (req, res, next) {
+    // Save course to database
+    coursebody.save(function (err) {
+      if (err) return console.error(err);
+    });
 
-		// Create new course
-		var course1 = new Course({
-			coursename: req.body.coursename,
-			courseid: req.body.coursenid,
-			progression: req.body.progression,
-			term: req.body.term,
-			syllabus: req.body.syllabus
-		});
-		console.log("test" + req.body.coursename);
+    var jsonObj = JSON.stringify(coursebody);
+    res.contentType("application/json");
+    res.send(jsonObj);
+  });
 
-		// Save course to database
-		course1.save(function (err) {
-			if (err) return console.error(err);
-		});
+  /******* DELETE course with unique ID ********/
+  router.delete("/:id", function (req, res, next) {
+    var id = req.params.id;
 
-		var jsonObj = JSON.stringify(course1);
-		res.contentType('application/json');
-		res.send(jsonObj);
-	});
+    // Delete course with specific id from db
+    Course.deleteOne({ _id: id }, function (err) {
+      if (err) return handleError(err);
+    });
 
-	/******* DELETE course with unique ID ********/
-	router.delete('/:id', function (req, res, next) {
-		var id = req.params.id;
-    
-		// Delete user _id from db
-		User.deleteOne({ "_id": id }, function (err) {
-			if (err) return handleError(err);
-		});
-    
-		// Get the new user list from db as response data
-		Course.find(function (err, courses) {
-			if (err) return console.error(err);
-    
-			var jsonObj = JSON.stringify(courses);
-			res.contentType('application/json');
-			res.send(jsonObj);
-		});
-	});
-
+    // Get the new courselist from db as response data
+    Course.find(function (err, courses) {
+      if (err) return console.error(err);
+      var jsonObj = JSON.stringify(courses);
+      res.contentType("application/json");
+      res.send(jsonObj);
+    });
+  });
 });
 
 module.exports = router;
